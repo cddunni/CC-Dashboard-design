cd69f42ffe4008261b96a7501e3c5b6195ca15c3
cddunni <oluwatimileyinakinpelu49@gmail.com>
2025-07-29 02:59:14 +0100

    complete marketing page implementation

---
+++ src/pages/marketing/SummaryCard.tsx
@@ -0,0 +1,44 @@
+import { Icon } from "@iconify/react";
+
+interface SummaryCardProps {
+  title: string;
+  value: string;
+  previous: string;
+  progress: number;
+  icon: string;
+}
+const SummaryCard = ({
+  title,
+  value,
+  previous,
+  progress,
+  icon,
+}: SummaryCardProps) => {
+  return (
+    <div className="border border-black min-w-40 p-4 bg-white rounded min-h-40">
+      <p className="font-semibold text-sm">{title}</p>
+      <div className="flex justify-between items-center mt-1">
+        <p className="text-2xl font-extrabold">{value}</p>
+        <Icon icon={icon} className="text-[#7CCF00]" width={24} height={24} />
+      </div>
+      <div className="flex justify-between mt-1 font-semibold text-extrasmall leading-3">
+        <div>
+          <p>Previous</p>
+          <p className="">{previous}</p>
+        </div>
+        <div>
+          <p>Progress</p>
+          <p
+            className={`${
+              progress < 0 ? "text-brand-red" : "text-brand-green"
+            }`}
+          >
+            {progress}%
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SummaryCard;
---
+++ src/pages/marketing/index.tsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+import DateFilter from "../../components/DateFilter";
+import SummaryCard from "./SummaryCard";
+import { LineChart, StackedBarChart } from "../../components/charts";
+import {
+  trafficSourceData,
+  acquisitionVersusCostData,
+  budgetByPlatform,
+} from "../../utils/dummy";
+import { Icon } from "@iconify/react";
+import { formatCurrency } from "../../utils/functions/generators";
+import { ProgressBar } from "../../components";
+
+const Marketing = () => {
+  const [currentDateFilter, setCurrentDateFilter] = useState(7);
+  return (
+    <div className="px-6">
+      <div className="flex justify-between items-center py-4">
+        <p className="text-lg font-bold">Marketing</p>
+        <DateFilter
+          handleDateFilter={setCurrentDateFilter}
+          selectedFilter={currentDateFilter}
+        />
+      </div>
+      <div className="grid grid-cols-2 gap-4">
+        <div className="grid grid-cols-2 gap-4">
+          <SummaryCard
+            title="Total Spend"
+            value="$8,765"
+            previous="$10,234"
+            progress={-14.32}
+            icon="bi:cash"
+          />
+          <SummaryCard
+            title="Visitor"
+            value="14,321"
+            previous="12,543"
+            progress={14.23}
+            icon="bi:people"
+          />
+          <SummaryCard
+            title="Acquisition"
+            value="$1,023"
+            previous="876"
+            progress={16.73}
+            icon="simple-line-icons:check"
+          />
+          <SummaryCard
+            title="Revenue"
+            value="$18,765"
+            previous="$15,432"
+            progress={21.67}
+            icon="streamline-plump:dollar-coin"
+          />
+        </div>
+        <div className="bg-white border-[1.5px] border-black rounded p-4 text-black font-medium">
+          <p className="font-semibold mb-2">Acquisition vs Cost</p>
+          <LineChart
+            labels={acquisitionVersusCostData.labels}
+            datasets={acquisitionVersusCostData.datasets}
+          />
+        </div>
+        <div className="bg-white border-[1.5px] border-black rounded p-4">
+          <p className="font-semibold mb-2">Traffic Source</p>
+          <StackedBarChart data={trafficSourceData} />
+        </div>
+        <div className="bg-white border-[1.5px] border-black rounded p-4 text-black font-medium">
+          <p className="font-semibold mb-2">Budget by Platform</p>
+          <div className="flex flex-col space-y-2">
+            {budgetByPlatform.map((budget) => (
+              <div key={budget.name} className="flex space-x-4 items-center">
+                <Icon icon={budget.icon} width={30} height={30} />
+                <div className="w-full">
+                  <div className="text-sm font-semibold flex justify-between mb-1.5">
+                    <p>Remaining {formatCurrency(budget.remaining)}</p>
+                    <p>{budget.percentage}%</p>
+                  </div>
+                  <ProgressBar percentage={budget.percentage}/>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Marketing;
